dadabfdadc6ccd2458deb3f1c22c161a45070256
Raikonif <raikonif@gmail.com>
2026-01-19 15:24:06 -0400

    feat: initial administrator interface updated

--- app/[locale]/administrators/types/administrator.ts
+++ app/[locale]/administrators/types/administrator.ts
@@ -1,11 +1,11 @@
 export interface Administrator {
   id: string;
+  user_id: string;
   name: string;
   email: string;
   phone: string;
   role: string;
   status: "Active" | "Inactive" | "Suspended";
-  registrationDate: string;
   is_active: boolean;
   created_at: string;
   updated_at: string;
